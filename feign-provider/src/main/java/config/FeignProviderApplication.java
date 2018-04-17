887391b6b8837527a9768044e78a685c6e83bcd3
chenxb <chenxb@asd-PC>
2018-04-17 15:06:36 +0800

    server eureka

---
+++ feign-provider/src/main/java/config/FeignProviderApplication.java
@@ -0,0 +1,23 @@
+package config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class FeignProviderApplication {
+
+	 public static void main(String[] args) {
+	        SpringApplication.run(FeignProviderApplication.class, args);
+	 }
+	 
+	 @RequestMapping(value="/testProvider" ,method = RequestMethod.GET)
+	 public String getProvider() {
+		 return "Feign Provider Success";
+	 }
+}
