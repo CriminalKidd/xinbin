b811b6e31efe9084bbbc80077f0e5842d7c48465
chenxb <chenxb@asd-PC>
2018-04-17 15:15:56 +0800

    添加注释

--- feign-consumer/src/main/java/config/FeignClientService.java
+++ feign-consumer/src/main/java/config/FeignClientService.java
@@ -1,11 +1,15 @@
 package config;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value="feign-provider")
+/**
+ * feign 客户端
+ * @author chenxb
+ *
+ */
+@FeignClient(value="feign-provider")//注明 feign 调用的服务名称
 public interface FeignClientService {
 
 	@RequestMapping(value="/testProvider",method=RequestMethod.GET)
--- feign-consumer/src/main/java/config/FeignController.java
+++ feign-consumer/src/main/java/config/FeignController.java
@@ -1,7 +1,6 @@
 package config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
